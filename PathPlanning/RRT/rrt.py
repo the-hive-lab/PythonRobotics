2eeb97c9511ade50fa02c1ef62798867ddf8989a
Vikhyat2603 <vikhyat2603@gmail.com>
2024-10-06 01:34:34 -0400

    add random seed to rrt, add verboes to rrt_star

--- PathPlanning/RRT/rrt.py
+++ PathPlanning/RRT/rrt.py
@@ -8,7 +8,6 @@ author: AtsushiSakai(@Atsushi_twi)
 
 import math
 import random
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -52,6 +51,7 @@ class RRT:
                  max_iter=500,
                  play_area=None,
                  robot_radius=0.0,
+                 random_seed=0,
                  ):
         """
         Setting Parameter
@@ -79,6 +79,7 @@ class RRT:
         self.obstacle_list = obstacle_list
         self.node_list = []
         self.robot_radius = robot_radius
+        random.seed(random_seed)
 
     def planning(self, animation=True):
         """
@@ -269,7 +270,8 @@ def main(gx=6.0, gy=10.0):
         rand_area=[-2, 15],
         obstacle_list=obstacleList,
         # play_area=[0, 10, 0, 14]
-        robot_radius=0.8
+        robot_radius=0.8,
+        goal_sample_rate=90
         )
     path = rrt.planning(animation=show_animation)
 
